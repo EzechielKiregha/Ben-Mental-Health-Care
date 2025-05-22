f16d95a3bb08ffc24a7c4a00cd2dad52b92cc574
EzechielKiregha <ekiregha@gmail.com>
2025-05-22 23:45:54 +0200

    mental health is deteriorating

--- support/src/main/java/drg/mentalhealth/support/controller/MentalHealthResourceController.java
+++ support/src/main/java/drg/mentalhealth/support/controller/MentalHealthResourceController.java
@@ -2,10 +2,14 @@ package drg.mentalhealth.support.controller;
 
 import drg.mentalhealth.support.model.MentalHealthResource;
 import drg.mentalhealth.support.model.ResourceType;
+import drg.mentalhealth.support.model.User;
 import drg.mentalhealth.support.repository.MentalHealthResourceRepository;
 import drg.mentalhealth.support.service.MentalHealthResourceService;
+import drg.mentalhealth.support.service.UserService;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +21,24 @@ public class MentalHealthResourceController {
     @Autowired
     private MentalHealthResourceService resourceService;
 
+    @Autowired
+    private UserService userService;
+
 
     @PostMapping("/upload")
     public ResponseEntity<?> uploadResource(
-            @RequestBody MentalHealthResource resource
+            @RequestBody MentalHealthResource resource,
+            @RequestParam Long userId,
+            @RequestParam Integer questionIndex
     ) {
+        Optional<User> user = userService.getUserById(userId);
+
+        if(user.isPresent()) {
+            resource.setTherapist(user.get());
+            resource.setQuestionIndex(questionIndex);
+        } else {
+            return ResponseEntity.status(400).body("User not found");
+        }
         MentalHealthResource savedResource = resourceService.saveResource(resource);
         if (savedResource == null) {
             return ResponseEntity.status(400).body("Failed to save resource");
@@ -29,7 +46,7 @@ public class MentalHealthResourceController {
         return ResponseEntity.ok(savedResource);
     }
 
-    
+
     @GetMapping
     public ResponseEntity<List<MentalHealthResource>> listResources(
             @RequestParam(required = false) List<Long> ids
