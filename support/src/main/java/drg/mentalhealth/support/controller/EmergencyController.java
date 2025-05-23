dc142eeb0b93477667cdbfdf67275d5fdf7d83b0
EzechielKiregha <ekiregha@gmail.com>
2025-05-24 00:01:34 +0200

    emergency

--- support/src/main/java/drg/mentalhealth/support/controller/EmergencyController.java
+++ support/src/main/java/drg/mentalhealth/support/controller/EmergencyController.java
@@ -1,7 +1,9 @@
 package drg.mentalhealth.support.controller;
 
 import drg.mentalhealth.support.model.EmergencyContact;
+import drg.mentalhealth.support.model.User;
 import drg.mentalhealth.support.repository.EmergencyContactRepository;
+import drg.mentalhealth.support.service.UserService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,25 +14,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/emergency")
 public class EmergencyController {
 
-    private final EmergencyContactRepository emergencyContactRepository;
+    @Autowired
+    private EmergencyContactRepository emergencyContactRepository;
 
-    public EmergencyController(EmergencyContactRepository emergencyContactRepository) {
-        this.emergencyContactRepository = emergencyContactRepository;
-    }
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public List<EmergencyContact> listAllContacts() {
         return emergencyContactRepository.findAll();
     }
 
-    @PostMapping
-    public ResponseEntity<EmergencyContact> addContact(@RequestBody EmergencyContact emergencyContact) {
+    @PostMapping("/new-contact-line")
+    public ResponseEntity<EmergencyContact> addContact(@RequestBody EmergencyContact emergencyContact, @RequestParam Long userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isPresent()) {
+            emergencyContact.setUser(user);
+        }else {
+            throw new RuntimeError("[No User ID provided]");
+        }
         return ResponseEntity.ok(emergencyContactRepository.save(emergencyContact));
     }
 }
